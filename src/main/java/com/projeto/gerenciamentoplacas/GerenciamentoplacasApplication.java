bb0c31d30fb9cb90753ed37c55d73549318d9afa
GerenciamPlacas <proj.cadastro.de.placas@gmail.com>
2019-03-12 22:54:43 -0300

    Projeto criado

---
+++ src/main/java/com/projeto/gerenciamentoplacas/GerenciamentoplacasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.gerenciamentoplacas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoplacasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoplacasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/gerenciamentoplacas/GerenciamentoplacasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projeto.gerenciamentoplacas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciamentoplacasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
